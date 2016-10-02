1f21ec5818b422b2601bd16185db2f83cb6151ed
lagital <borisenkopp@yandex.ru>
2016-10-02 20:32:06 +0300

    SystemPicker updates

---
+++ app/src/main/java/com/sam/team/character/design/ActivityEditSystem.java
@@ -0,0 +1,21 @@
+package com.sam.team.character.design;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pborisenko on 10/2/2016.
+ */
+
+public class ActivityEditSystem extends AppCompatActivity {
+
+    public static final String SYSTEM_NAME_EXTRA = "SYSTEM_NAME_EXTRA";
+    public static final String SYSTEM_VERSION_EXTRA = "SYSTEM_VERSION_EXTRA";
+    public static final String SYSTEM_VERSION_DFLT_EXTRA = "SYSTEM_VERSION_EXTRA";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/sam/team/character/design/ActivitySystemPicker.java
+++ app/src/main/java/com/sam/team/character/design/ActivitySystemPicker.java
@@ -1,14 +1,17 @@
 package com.sam.team.character.design;
 
 import android.content.DialogInterface;
-import android.support.design.widget.CoordinatorLayout;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -31,8 +34,6 @@ public class ActivitySystemPicker extends AppCompatActivity {
     private FloatingActionButton mMainFAB;
     private FloatingActionButton mAddMiniFAB;
     private FloatingActionButton mLoadMiniFAB;
-    private CoordinatorLayout.LayoutParams addLayoutParams;
-    private CoordinatorLayout.LayoutParams loadLayoutParams;
     private Toolbar mToolbar;
 
     Animation miniFABShow;
@@ -57,14 +58,11 @@ public class ActivitySystemPicker extends AppCompatActivity {
         mAddMiniFAB = (FloatingActionButton) findViewById(R.id.fab_add);
         mLoadMiniFAB = (FloatingActionButton) findViewById(R.id.fab_load);
 
-        addLayoutParams = (CoordinatorLayout.LayoutParams) mAddMiniFAB.getLayoutParams();
-        loadLayoutParams = (CoordinatorLayout.LayoutParams) mLoadMiniFAB.getLayoutParams();
-
         mMainFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    animateMiniFAB(mAddMiniFAB, mAddMiniFAB.isClickable(), addLayoutParams, 0.99, 0);
-                    animateMiniFAB(mLoadMiniFAB, mLoadMiniFAB.isClickable(), loadLayoutParams, 0.7, 0.7);
+                    animateMiniFAB(mAddMiniFAB, mAddMiniFAB.isClickable());
+                    animateMiniFAB(mLoadMiniFAB, mLoadMiniFAB.isClickable());
             }
         });
 
@@ -75,33 +73,41 @@ public class ActivitySystemPicker extends AppCompatActivity {
                         R.layout.dialog_new_system, null);
                 final EditText name = (EditText) l.findViewById(R.id.name);
                 final EditText version = (EditText) l.findViewById(R.id.version);
-                name.setOnClickListener(new View.OnClickListener() {
+                name.setOnTouchListener(new View.OnTouchListener() {
                     @Override
-                    public void onClick(View v) {
+                    public boolean onTouch(View v, MotionEvent event) {
                         if (name.getText().toString().equalsIgnoreCase(
                                 getResources().getString(R.string.new_system_dflt_name))) {
                             name.setText("");
+                            name.setTextColor(ContextCompat.
+                                    getColor(ActivitySystemPicker.this, R.color.colorPrimary));
+                            Log.d(TAG, "Fill system name");
                         }
+                        return false;
                     }
                 });
 
-                version.setOnClickListener(new View.OnClickListener() {
+                version.setOnTouchListener(new View.OnTouchListener() {
                     @Override
-                    public void onClick(View v) {
-                        if (name.getText().toString().equalsIgnoreCase(
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (version.getText().toString().equalsIgnoreCase(
                                 getResources().getString(R.string.new_system_dflt_version))) {
-                            name.setText("");
+                            version.setText("");
+                            version.setTextColor(ContextCompat.
+                                    getColor(ActivitySystemPicker.this, R.color.colorPrimary));
+                            Log.d(TAG, "Fill version");
                         }
+                        return false;
                     }
                 });
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(ActivitySystemPicker.this);
                 builder.setView(l);
-
+                builder.setTitle(R.string.new_system_dialog_title);
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // do nothing
+                        // do nothing, see dialog.getButton
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -125,12 +131,19 @@ public class ActivitySystemPicker extends AppCompatActivity {
                         RPSystem rps;
                         if (!e_name.isEmpty() && !e_name.equalsIgnoreCase(
                                 getResources().getString(R.string.new_system_dflt_name))) {
+
+                            Intent intent = new Intent(ActivitySystemPicker.this, ActivityEditSystem.class);
+                            intent.putExtra(ActivityEditSystem.SYSTEM_NAME_EXTRA, e_name);
+
                             if (e_version.isEmpty() || e_version.equalsIgnoreCase(
                                     getResources().getString(R.string.new_system_dflt_version))) {
-                                rps = new RPSystem(e_name);
+                                intent.putExtra(ActivityEditSystem.SYSTEM_VERSION_EXTRA,
+                                        ActivityEditSystem.SYSTEM_VERSION_DFLT_EXTRA);
+                            } else {
+                                intent.putExtra(ActivityEditSystem.SYSTEM_VERSION_EXTRA,
+                                        e_version);
                             }
-                            rps = new RPSystem(e_name, e_version);
-                            //TODO: going to system creation
+                            startActivity(intent);
                         } else {
                             Toast.makeText(ActivitySystemPicker.this,
                                     getResources().getString(R.string.new_system_empty_name),
@@ -152,7 +165,7 @@ public class ActivitySystemPicker extends AppCompatActivity {
 
         /*debug*/
         if (BuildConfig.DEBUG) {
-            for (int i = 0; i < 15; i++) {
+            for (int i = 0; i < 5; i++) {
                 rpsl.add(new RPSystem("TEST", Integer.toString(i)));
             }
         }
@@ -162,8 +175,7 @@ public class ActivitySystemPicker extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
     }
 
-    private void animateMiniFAB(FloatingActionButton miniFAB, Boolean hide,
-                                CoordinatorLayout.LayoutParams params, double posWidth, double posHeight) {
+    private void animateMiniFAB(FloatingActionButton miniFAB, Boolean hide) {
         miniFAB.startAnimation(hide ? miniFABHide : miniFABShow);
         miniFAB.setClickable(!hide);
     }
